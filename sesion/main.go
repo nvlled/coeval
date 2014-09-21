136c100e1ccf47d09cdd5235b4f41742e874290a
ron <nvlled@gmail.com>
2014-09-21 18:52:30 +0800

    Add sesion functions

--- sesion/main.go
+++ sesion/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gorilla/sessions"
 	"github.com/gorilla/context"
 	//"strings"
+	"nvlled/goeval/fora"
 )
 
 const (
@@ -15,6 +16,27 @@ const (
 )
 
 var store = sessions.NewCookieStore([]byte("supersecretpassword"))
+
+func SetUsername(username string, w ht.ResponseWriter, r *ht.Request) {
+	s,_ := store.Get(r, Name)
+	s.Values[key.Username] = username
+	s.Save(r, w)
+}
+
+func FlashSet(w ht.ResponseWriter, r *ht.Request, key string, val interface{}) {
+	s,_ := store.Get(r, Name)
+	s.AddFlash(val, key)
+	s.Save(r, w)
+}
+
+func FlashGet(r *ht.Request, key string) interface{} {
+	s,_ := store.Get(r, Name)
+	fs := s.Flashes(key)
+	if len(fs) > 0 {
+		return fs[0]
+	}
+	return nil
+}
 
 func Username(r *ht.Request) string {
 	s,_ := store.Get(r, Name)
@@ -25,11 +47,17 @@ func Username(r *ht.Request) string {
 	return ""
 }
 
+func User(r *ht.Request) fora.User {
+	u := context.Get(r, key.User)
+	switch t := u.(type) {
+		case fora.User: return t
+	}
+	return fora.Anonymous()
+}
+
 func Merge(r *ht.Request, data rend.Data) rend.Data {
 	data["username"] = Username(r)
 	data["user"] = context.Get(r, key.User)
 	return data
 }
-
-
 
