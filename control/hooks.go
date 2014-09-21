fae0d8e069e21abff332e90c2e444d5cbc5d2ba4
ron <nvlled@gmail.com>
2014-09-21 18:57:07 +0800

    Update hooks

--- control/hooks.go
+++ control/hooks.go
@@ -9,6 +9,8 @@ import (
 	"nvlled/goeval/sesion"
 	"nvlled/goeval/sesion/key"
 	"nvlled/rut"
+	"nvlled/rule"
+	"log"
 )
 
 type handler func(ht.ResponseWriter, *ht.Request)
@@ -23,18 +25,14 @@ func AttachUser(r *ht.Request) {
 }
 
 func notAdmin(r *ht.Request) bool {
-	user := context.Get(r, key.User)
-	switch user.(type) {
-	case fora.User:
-		return (user.(fora.User)).Kind() != fora.Admin
-	}
-	return false
+	user := sesion.User(r)
+	return user.Kind() != fora.Admin
 }
 
 var RequireAdmin = rut.Guard{
 	Reject: notAdmin,
 	Handler: func(w ht.ResponseWriter, r *ht.Request) {
-		rend.Render(w, r, rend.Data{
+		rend.RenderRoute("error", w, r, rend.Data{
 			"error" : fora.AdminError,
 		})
 	},
@@ -44,19 +42,23 @@ func CatchError(handler ht.Handler) ht.Handler {
 	return ht.HandlerFunc(func(w ht.ResponseWriter, r *ht.Request) {
 		defer func() {
 			if err := recover(); err != nil {
-				// Should probably handle only relevant errors
-				rend.Render(w, r, rend.Data{
-					"error" : err,
-				})
-				// should be able to infer what template
-				// to render based on the context
+				var render = func(err interface{}) {
+					rend.RenderRoute("error", w, r, rend.Data{
+						"error" : err,
+					})
+				}
+
+				switch t := err.(type) {
+				case rule.Error : render(t)
+				case map[string]interface{}: render(t)
+				case string: render(rule.AnError("__msg", t))
+				default: log.Println(err)
+				}
 			}
 		} ()
 		handler.ServeHTTP(w, r)
 	})
 }
-
-
 
 
 
