7ccccadaff0599b473fc890ac1ad152c7f1909ef
ron <nvlled@gmail.com>
2014-09-30 18:11:53 +0800

    Re-indent files

--- static/public/scripts/chan.js
+++ static/public/scripts/chan.js
@@ -1,103 +1,103 @@
 (function() {
 
-this.addEventListener("load", function() {
-	intfmain.init();
-	initForm();
-});
+	this.addEventListener("load", function() {
+		intfmain.init();
+		initForm();
+	});
 
-function buildThread(posts) {
-}
+	function buildThread(posts) {
+	}
 
-var sel = function(s, node) {
-	if (!node)
-		node = document;
-	return node.querySelector(s);
-}
-this.sel = sel;
+	var sel = function(s, node) {
+		if (!node)
+			node = document;
+		return node.querySelector(s);
+	}
+	this.sel = sel;
 
-function initForm() {
-	var form = sel("#board-form");
-	var info = sel("#info");
-	var bid = sel("#bid");
-	var tid = sel("#tid");
+	function initForm() {
+		var form = sel("#board-form");
+		var info = sel("#info");
+		var bid = sel("#bid");
+		var tid = sel("#tid");
 
-	var button = sel("button", form);
-	button.onclick = function() {
-		bid.value = bid.value.trim();
-		tid.value = tid.value.trim();
+		var button = sel("button", form);
+		button.onclick = function() {
+			bid.value = bid.value.trim();
+			tid.value = tid.value.trim();
 
-		var url
-		if (!bid.value || !tid.value) {
-			url = "/4chan/testdata";
-			info.textContent = "Loading from testdata...";
-		} else {
-			url = "/4chan/"+bid.value+"/"+tid.value;
-			info.textContent = "Loading";
-		}
-
-		fetchResource(url, function(text) {
-			try {
-				var t = JSON.parse(text);
-				loadThread(t);
-				info.textContent = "";
-			} catch(e) {
-				fetchFailed("not a json");
+			var url
+			if (!bid.value || !tid.value) {
+				url = "/4chan/testdata";
+				info.textContent = "Loading from testdata...";
+			} else {
+				url = "/4chan/"+bid.value+"/"+tid.value;
+				info.textContent = "Loading";
 			}
-		}, fetchFailed);
-	}
 
-	function fetchFailed(e) {
-		info.textContent = "Thread not found";
-		console.log("failed to fetch resource ", e);
-	}
-}
-
-function loadThread(threadData) {
-	var container = document.querySelector("#chan-posts");
-	var dptPosts = threadData.posts.map(fromChan);
-	intfmain.buildThread(dptPosts, container);
-}
-
-function createThreadURL(bid, tid) {
-}
-
-function fetchResource(url, succfn, errorfn) {
-	console.log("*** fetching", url)
-	var req = new XMLHttpRequest();
-	req.open("GET", url);
-	req.onload = function() {
-		succfn(req.responseText);
-	}
-	req.addEventListener("error", errorfn);
-	req.send();
-}
-
-var compat = /<a .*>&gt;&gt;(.*)<\/a>/g;
-var brpat = /<br>/g;
-var quotepat = /<span class="quote">&gt;(.*)<\/span>/g
-var decodeHTML = (function() {
-	var node = document.createElement("div");
-	return function(s) {
-		node.innerHTML = s;
-		if (node.childNodes.length > 0) {
-			var v = node.childNodes[0].nodeValue;
-			if (v)
-				return v;
+			fetchResource(url, function(text) {
+				try {
+					var t = JSON.parse(text);
+					loadThread(t);
+					info.textContent = "";
+				} catch(e) {
+					fetchFailed("not a json");
+				}
+			}, fetchFailed);
 		}
-		return s;
-	}
-})();
 
-function fromChan(data) {
-	var body = data.com || data.sub || "";
-	body = body.replace(compat, function(_, id) { return ">>"+id; });
-	body = body.replace(brpat, "\n");
-	body = body.replace(quotepat, function(_, quote) { return ">"+quote });
-	return {
-		id: data.no,
-		body: decodeHTML(body),
+		function fetchFailed(e) {
+			info.textContent = "Thread not found";
+			console.log("failed to fetch resource ", e);
+		}
 	}
-}
+
+	function loadThread(threadData) {
+		var container = document.querySelector("#chan-posts");
+		var dptPosts = threadData.posts.map(fromChan);
+		intfmain.buildThread(dptPosts, container);
+	}
+
+	function createThreadURL(bid, tid) {
+	}
+
+	function fetchResource(url, succfn, errorfn) {
+		console.log("*** fetching", url)
+		var req = new XMLHttpRequest();
+		req.open("GET", url);
+		req.onload = function() {
+			succfn(req.responseText);
+		}
+		req.addEventListener("error", errorfn);
+		req.send();
+	}
+
+	var compat = /<a .*>&gt;&gt;(.*)<\/a>/g;
+	var brpat = /<br>/g;
+	var quotepat = /<span class="quote">&gt;(.*)<\/span>/g
+	var decodeHTML = (function() {
+		var node = document.createElement("div");
+		return function(s) {
+			node.innerHTML = s;
+			if (node.childNodes.length > 0) {
+				var v = node.childNodes[0].nodeValue;
+				if (v)
+					return v;
+			}
+			return s;
+		}
+	})();
+
+	function fromChan(data) {
+		var body = data.com || data.sub || "";
+		body = body.replace(compat, function(_, id) { return ">>"+id; });
+		body = body.replace(brpat, "\n");
+		body = body.replace(quotepat, function(_, quote) { return ">"+quote });
+		return {
+			id: data.no,
+			body: decodeHTML(body),
+		}
+	}
 
 
 })();
--- static/public/scripts/intf-main.js
+++ static/public/scripts/intf-main.js
@@ -1,208 +1,208 @@
 (function(root) {
 
-// exports
-root.intfmain = {
-	buildThread: buildThread,
-	init: function() {
-		initPostTempl();
-	},
-}
-
-var postdb = {};
-function getPost(id) { return postdb[id] }
-
-var lib = intf.create({
-	newNode: newPostNode,
-	getPost: getPost,
-	postvisit: function(postmap) {
-		// TODO: Replace super magical string literals
-		if (postmap.type == "parent") {
-			var post = postmap.targetPost;
-			post.node.scrollIntoView();
-		}
+	// exports
+	root.intfmain = {
+		buildThread: buildThread,
+		init: function() {
+			initPostTempl();
+		},
 	}
-});
 
-function buildThread(posts, container) {
-	if (!container)
-		container = document.body;
+	var postdb = {};
+	function getPost(id) { return postdb[id] }
 
-	var postIds = [];
-	posts.forEach(function(postData) {
-		var post = lib.newPost(postData);
-		postdb[post.id] = post;
-		postIds.push(post.id);
-		container.appendChild(post.node);
-	});
-
-	var prevSib = {};
-	for (var i = 0; i < postIds.length; i++) {
-		var prev = getPost(postIds[i-1]);
-		var post = getPost(postIds[i]);
-		var next = getPost(postIds[i+1]);
-
-		post.norder.prev = prev;
-		post.norder.next = next;
-
-		linkToParentNodes(post)
-		linkSiblings(prevSib, post);
-	}
-}
-
-//function init2() {
-//	initPostTempl();
-//
-//	var postIds = [];
-//	posts.forEach(function(postData) {
-//		var post = lib.newPost(postData);
-//		postdb[post.id] = post;
-//		postIds.push(post.id);
-//		document.body.appendChild(post.node);
-//	});
-//
-//	var prevSib = {};
-//	for (var i = 0; i < postIds.length; i++) {
-//		var prev = getPost(postIds[i-1]);
-//		var post = getPost(postIds[i]);
-//		var next = getPost(postIds[i+1]);
-//
-//		post.norder.prev = prev;
-//		post.norder.next = next;
-//
-//		linkToParentNodes(post)
-//		linkSiblings(prevSib, post);
-//
-//	}
-//}
-
-function linkToParentNodes(post) {
-	post.parentIds.forEach(function(parentId) {
-		var parentNode = document.getElementById("p"+parentId);
-		var postlink = intf.childlink(post.id, parentId);
-		if (parentNode) {
-			addChildlink(parentNode, postlink);
+	var lib = intf.create({
+		newNode: newPostNode,
+		getPost: getPost,
+		postvisit: function(postmap) {
+			// TODO: Replace super magical string literals
+			if (postmap.type == "parent") {
+				var post = postmap.targetPost;
+				post.node.scrollIntoView();
+			}
 		}
 	});
-}
 
-function linkSiblings(prevSib, post) {
-	var pids = post.parentIds;
-	pids.forEach(function(pid) {
-		var post1 = prevSib[pid];
-		var post2 = post;
-		post.parentIds.forEach(function(pid) {
-			if (post1)
-				post1.nextSib[pid] = post2;
-			post2.prevSib[pid] = post1;
-			prevSib[pid] = post2;
+	function buildThread(posts, container) {
+		if (!container)
+			container = document.body;
+
+		var postIds = [];
+		posts.forEach(function(postData) {
+			var post = lib.newPost(postData);
+			postdb[post.id] = post;
+			postIds.push(post.id);
+			container.appendChild(post.node);
 		});
-	});
-}
 
-var postTempl;
-function initPostTempl() {
-	postTempl = document.querySelector(".template .post");
-	postTempl.querySelector(".post-body").innerHTML = "";
-	postTempl.remove();
-}
+		var prevSib = {};
+		for (var i = 0; i < postIds.length; i++) {
+			var prev = getPost(postIds[i-1]);
+			var post = getPost(postIds[i]);
+			var next = getPost(postIds[i+1]);
 
-function addChildlink(postNode, postlink) {
-	var node = createPostLinkNode(postlink.targetId, PlinkType.CHILD);
-	node.onclick = lib.createLinkHandler(postlink);
-	var replies = postNode.querySelector(".post-replies");
-	replies.appendChild(node);
-}
+			post.norder.prev = prev;
+			post.norder.next = next;
 
-function newPostNode(data) {
-	var postNode = postTempl.cloneNode(true);
-	postNode.setAttribute("id", "p"+data.id);
-	postNode.querySelector(".post-id").textContent = data.id;
-	postNode.querySelector(".post-anchor").href = "#p"+data.id;
-	var bodyNode = postNode.querySelector(".post-body");
+			linkToParentNodes(post)
+			linkSiblings(prevSib, post);
+		}
+	}
 
-	var parentIds = parsePostBody(data, bodyNode);
+	//function init2() {
+	//	initPostTempl();
+	//
+	//	var postIds = [];
+	//	posts.forEach(function(postData) {
+	//		var post = lib.newPost(postData);
+	//		postdb[post.id] = post;
+	//		postIds.push(post.id);
+	//		document.body.appendChild(post.node);
+	//	});
+	//
+	//	var prevSib = {};
+	//	for (var i = 0; i < postIds.length; i++) {
+	//		var prev = getPost(postIds[i-1]);
+	//		var post = getPost(postIds[i]);
+	//		var next = getPost(postIds[i+1]);
+	//
+	//		post.norder.prev = prev;
+	//		post.norder.next = next;
+	//
+	//		linkToParentNodes(post)
+	//		linkSiblings(prevSib, post);
+	//
+	//	}
+	//}
 
-	return { node: postNode, parentIds: parentIds };
-}
+	function linkToParentNodes(post) {
+		post.parentIds.forEach(function(parentId) {
+			var parentNode = document.getElementById("p"+parentId);
+			var postlink = intf.childlink(post.id, parentId);
+			if (parentNode) {
+				addChildlink(parentNode, postlink);
+			}
+		});
+	}
 
-function createPostLinkNode(id, type, handler) {
-	var a = document.createElement("a");
-	a.href = '#p'+id;
-	a.classList.add("postlink");
-	a.classList.add(""+type);
-	a.textContent = ">>"+id;
-	a.onclick = handler;
-	return a;
-}
+	function linkSiblings(prevSib, post) {
+		var pids = post.parentIds;
+		pids.forEach(function(pid) {
+			var post1 = prevSib[pid];
+			var post2 = post;
+			post.parentIds.forEach(function(pid) {
+				if (post1)
+					post1.nextSib[pid] = post2;
+				post2.prevSib[pid] = post1;
+				prevSib[pid] = post2;
+			});
+		});
+	}
 
-var PlinkType = {
-	PARENT: "parent",
-	CHILD: "child",
-}
+	var postTempl;
+	function initPostTempl() {
+		postTempl = document.querySelector(".template .post");
+		postTempl.querySelector(".post-body").innerHTML = "";
+		postTempl.remove();
+	}
 
-function br() {
-	return document.createElement("br");
-}
+	function addChildlink(postNode, postlink) {
+		var node = createPostLinkNode(postlink.targetId, PlinkType.CHILD);
+		node.onclick = lib.createLinkHandler(postlink);
+		var replies = postNode.querySelector(".post-replies");
+		replies.appendChild(node);
+	}
 
-function meymey() {
-	var span = document.createElement("span");
-	span.classList.add("maymay-arrow");
-	return span;
-}
+	function newPostNode(data) {
+		var postNode = postTempl.cloneNode(true);
+		postNode.setAttribute("id", "p"+data.id);
+		postNode.querySelector(".post-id").textContent = data.id;
+		postNode.querySelector(".post-anchor").href = "#p"+data.id;
+		var bodyNode = postNode.querySelector(".post-body");
 
-function maymayArrow(text) {
-	var span = document.createElement("span");
-	span.classList.add("maymay-arrow");
-	span.textContent = text;
-	return span;
-}
+		var parentIds = parsePostBody(data, bodyNode);
 
-function textNode(t) {
-	return document.createTextNode(t)
-}
+		return { node: postNode, parentIds: parentIds };
+	}
 
-function spanNode(t) {
-	var span = document.createElement("span");
-	span.innerHTML = t;
-	return span
-}
+	function createPostLinkNode(id, type, handler) {
+		var a = document.createElement("a");
+		a.href = '#p'+id;
+		a.classList.add("postlink");
+		a.classList.add(""+type);
+		a.textContent = ">>"+id;
+		a.onclick = handler;
+		return a;
+	}
 
-function parsePostBody(postData, node) {
-	var parentIds = [];
-	var lines = postData.body.split("\n");
-	lines.forEach(function(line) {
-		if (line[0] == '>' && line[1] != '>') {
-			node.appendChild(maymayArrow(line.substr(0)));
+	var PlinkType = {
+		PARENT: "parent",
+		CHILD: "child",
+	}
+
+	function br() {
+		return document.createElement("br");
+	}
+
+	function meymey() {
+		var span = document.createElement("span");
+		span.classList.add("maymay-arrow");
+		return span;
+	}
+
+	function maymayArrow(text) {
+		var span = document.createElement("span");
+		span.classList.add("maymay-arrow");
+		span.textContent = text;
+		return span;
+	}
+
+	function textNode(t) {
+		return document.createTextNode(t)
+	}
+
+	function spanNode(t) {
+		var span = document.createElement("span");
+		span.innerHTML = t;
+		return span
+	}
+
+	function parsePostBody(postData, node) {
+		var parentIds = [];
+		var lines = postData.body.split("\n");
+		lines.forEach(function(line) {
+			if (line[0] == '>' && line[1] != '>') {
+				node.appendChild(maymayArrow(line.substr(0)));
+				node.appendChild(br());
+				// Fix: don't return, just add styling on the current line
+				return;
+			}
+			var matched = false;
+			var pat = /(.*)(>>\d+)(.*)/g;
+			while(true) {
+				var m = pat.exec(line);
+				if (m == null)
+					break;
+				matched = true;
+				var parentId = m[2].substr(2);
+				var postlinkNode = createPostLinkNode(parentId, PlinkType.PARENT);
+
+				var postlink = intf.parentlink(parentId, postData.id);
+				postlinkNode.onclick = lib.createLinkHandler(postlink);
+
+				parentIds.push(parentId)
+
+				node.appendChild(textNode(m[1]));
+				node.appendChild(postlinkNode);
+				node.appendChild(textNode(m[3]));
+			}
+			if (!matched)
+				node.appendChild(textNode(line));
+
 			node.appendChild(br());
-			// Fix: don't return, just add styling on the current line
-			return;
-		}
-		var matched = false;
-		var pat = /(.*)(>>\d+)(.*)/g;
-		while(true) {
-			var m = pat.exec(line);
-			if (m == null)
-				break;
-			matched = true;
-			var parentId = m[2].substr(2);
-			var postlinkNode = createPostLinkNode(parentId, PlinkType.PARENT);
-
-			var postlink = intf.parentlink(parentId, postData.id);
-			postlinkNode.onclick = lib.createLinkHandler(postlink);
-
-			parentIds.push(parentId)
-
-			node.appendChild(textNode(m[1]));
-			node.appendChild(postlinkNode);
-			node.appendChild(textNode(m[3]));
-		}
-		if (!matched)
-			node.appendChild(textNode(line));
-
-		node.appendChild(br());
-	});
-	return parentIds;
-}
+		});
+		return parentIds;
+	}
 
 })(this);
 
