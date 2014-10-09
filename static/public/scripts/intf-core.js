25cf8fa19cb01f943924477afdefc860419c6cd7
ron <nvlled@gmail.com>
2014-10-09 19:02:13 +0800

    Implement module instantiation

--- static/public/scripts/intf-core.js
+++ static/public/scripts/intf-core.js
@@ -1,85 +1,71 @@
 (function(root) {
 
-	// exports
-	root.intf = {
-		childlink:	childlink,
-		parentlink: parentlink,
-		create: create,
-		getSiblings: getSiblings,
-	}
-
 	var PAGE_SIZE = 3;
 
-	//post {
-	//	id int
-	//	node HtmlNode
-	//  norder  {next, prev} (never modified)
-	//  page	{start, end}
-	//  nextpost post
-	//  indented
-	//
-	//	nextsib: {
-	//		pid1: post1,
-	//		pid2: post2,
-	//		pid3: post3,
-	//	}
-	//  prevsib: {
-	//		pid1: post1,
-	//		pid2: post2,
-	//  }
-	//
-	// pids: [post]
-	// firstchild: post,
-	// lastchild: post,
-	//}
-
-	//postlink {
-	//	type string
-	//	sourcePost post
-	//	targetPost post
-	//}
-
-	function create(opts) {
-		var newNode = opts.newNode;
-		var getPost = opts.getPost;
-		var postvisit = opts.postvisit;
-		return {
-			newPost: function(data) {
-				// TODO: rename newNode
-				var t = newNode(data);
-				console.assert(newNode, "need a node creator");
-				return {
-					id:		   data.id,
-					node:	   t.node,
-					norder:    {next:  null, prev: null},
-					sibling:   {next:  null, prev: null},
-					page:	   {start: null, end:  null},
-					nextpost:  null,
-					prevpost:  null,
-					indented:  false,
-					parentIds: t.parentIds,
-					prevSib:   {},
-					nextSib:   {},
-					firstchild: null,
-					lastchild:  null,
-					// I relied too much on linked lists
-					// Fix: Add some structure
-				}
-			},
-			createLinkHandler: function(postlink) {
-				return function() {
-					var postmap = mapPostId(postlink, getPost)
-					visitLink(postmap);
-					if (postvisit)
-						postvisit(postmap);
-					return false;
-				}
-			}
-		}
-
+	try {
+		// running on node
+		module;
+		module.exports = new Module();
+	} catch(e) {
+		// running on browser
+		root.intf = new Module();
 	}
 
-	function getSiblings(post, pid) {
+	function Module(opts) {
+		opts = opts || {};
+
+		// default to using own's modules postdb
+		this.getPost = opts.getPost;
+
+		this.newNode = opts.newNode;
+		this.postvisit = opts.postvisit;
+		this.parsePostIds = opts.parsePostIds;
+		this.events = [];
+
+		this.lastCreatedPost = null;
+		this.prevChildId = {};
+	}
+
+	var M = Module.prototype;
+
+	M.newModule = function(opts) {
+		return new Module(opts);
+	}
+
+	M.newPost = function(data) {
+		// TODO: rename newNode
+		var t = this.newNode(data);
+		console.assert(newNode, "need a node creator");
+		return {
+			id:		   data.id,
+			node:	   t.node,
+			norder:    {next:  null, prev: null},
+			sibling:   {next:  null, prev: null},
+			page:	   {start: null, end:  null},
+			nextpost:  null,
+			prevpost:  null,
+			indented:  false,
+			parentIds: t.parentIds,
+			prevSib:   {},
+			nextSib:   {},
+			firstchild: null,
+			lastchild:  null,
+			// I relied too much on linked lists
+			// Fix: Add some structure
+		}
+	}
+
+	M.createLinkHandler = function(postlink) {
+		return function() {
+			var postmap = this.mapPostId(postlink);
+			this.visitLink(postmap);
+			if (postvisit)
+				postvisit(postmap);
+			return false;
+		}
+	}
+
+	M.getSiblings = function(post, pid) {
 		var posts = [];
 		while(post) {
 			posts.push(post);
@@ -88,7 +74,7 @@
 		return posts;
 	}
 
-	function mapPostId(postlink, getPost) {
+	M.mapPostId = function(postlink, getPost) {
 		return {
 			type:		postlink.type,
 			targetPost: getPost(postlink.targetId),
@@ -96,7 +82,7 @@
 		}
 	}
 
-	function childlink(targetId, sourceId) {
+	M.childlink = function(targetId, sourceId) {
 		return {
 			type:		"child",
 			targetId: targetId,
@@ -104,7 +90,7 @@
 		}
 	}
 
-	function parentlink(targetId, sourceId) {
+	M.parentlink = function(targetId, sourceId) {
 		return {
 			type:		"parent",
 			targetId: targetId,
@@ -112,53 +98,53 @@
 		}
 	}
 
-	function insertAfter(insertedNode, node) {
+	M.insertAfter = function(insertedNode, node) {
 		var parentNode = node.parentNode;
 		parentNode.insertBefore(insertedNode, node.nextSibling)
 	}
 
-	function insertBefore(insertedNode, node) {
+	M.insertBefore = function(insertedNode, node) {
 		var parentNode = node.parentNode;
 		parentNode.insertBefore(insertedNode, node);
 	}
 
-	function appendChild(parent, node) {
+	M.appendChild = function(parent, node) {
 		parent.appendChild(node);
 	}
 
-	function attachPosts(linktype, parent, post) {
+	M.attachPosts = function(linktype, parent, post) {
 		if (parent.nextpost == post && linktype == "parent")
 			return;
 
-		restoreSupthread(parent);
-		clearSubthread(parent);
+		this.restoreSupthread(parent);
+		this.clearSubthread(parent);
 
-		if (!isIndented(post) && parent.nextpost == post)
-			attachSiblings(parent, post);
-		else if (isIndented(post) || inNorder(post))
-			attachSiblings(parent, post);
+		if (!this.isIndented(post) && parent.nextpost == post)
+			this.attachSiblings(parent, post);
+		else if (this.isIndented(post) || inNorder(post))
+			this.attachSiblings(parent, post);
 		else
-			attachSubthread(parent, post);
+			this.attachSubthread(parent, post);
 
 		// Parents are always undented
-		undent(parent);
+		this.undent(parent);
 	}
 
-	function attachSubthread(parent, post) {
+	M.attachSubthread = function(parent, post) {
 		var post1 = parent;
 		var post2 = post;
 		console.log("**attaching subthread", post1.id, "->", post2.id);
 
 		while (post2) {
-			insertAfter(post2.node, post1.node);
-			setNextPost(post1, post2);
+			this.insertAfter(post2.node, post1.node);
+			this.setNextPost(post1, post2);
 			post2.prevpost = post1;
 			post1 = post2;
 			post2 = post2.nextpost;
 		}
 	}
 
-	function attachSiblings(parent, post) {
+	M.attachSiblings = function(parent, post) {
 		var post1 = parent;
 		var post2 = post;
 		var start = post2;
@@ -167,10 +153,10 @@
 		var i = 0;
 		while (i < PAGE_SIZE) {
 			// Post in pages are always indented
-			indent(post2);
-			insertAfter(post2.node, post1.node);
+			this.indent(post2);
+			this.insertAfter(post2.node, post1.node);
 
-			setNextPost(post1, post2);
+			this.setNextPost(post1, post2);
 			post2.prevpost = post1;
 			post1 = post2;
 			post2 = post2.nextSib[parent.id];
@@ -182,14 +168,14 @@
 		parent.page.end = post2;
 	}
 
-	function visitParent(postlink) {
+	M.visitParent = function(postlink) {
 		console.log("**visiting parent", postlink)
 		var parent = postlink.targetPost;
 		var post = postlink.sourcePost;
-		attachPosts("parent", parent, post);
+		this.attachPosts("parent", parent, post);
 	}
 
-	function visitChild(postlink) {
+	M.visitChild = function(postlink) {
 		console.log("**visiting child", postlink);
 		var child = postlink.targetPost;
 		var post = postlink.sourcePost;
@@ -199,25 +185,25 @@
 	// Connects the post (or subthread)
 	// refered to by postlink.sourceId to the
 	// post refered to by the postlink.targetId
-	function visitLink(postlink) {
+	M.visitLink = function(postlink) {
 		switch(postlink.type) {
-			case "parent" : visitParent(postlink); break;
-			case "child"  : visitChild(postlink); break;
+			case "parent" : this.visitParent(postlink); break;
+			case "child"  : this.visitChild(postlink); break;
 		}
 	}
 
-	function nextPage(post) {
-		if (isUndented(post)) {
+	M.nextPage = function(post) {
+		if (this.isUndented(post)) {
 			var child = post.page.end;
 			var next = child.sibilings.next;
 			if (next) { // avoid showing a blank page
-				attachSiblings(post, next); // exclude child
+				this.attachSiblings(post, next); // exclude child
 			}
 		}
 	}
 
-	function prevPage(post) {
-		if (isUndented(post)) {
+	M.prevPage = function(post) {
+		if (this.isUndented(post)) {
 			var child = post.page.start;
 			// TODO: Create attachSiblings that goes backwards
 			var i = 0;
@@ -229,57 +215,57 @@
 				i++;
 			}
 			if (prev) {
-				attachSiblings(post, next);
+				this.attachSiblings(post, next);
 			}
 		}
 	}
 
-	function restoreNorder(post) {
+	M.restoreNorder = function(post) {
 		console.log("**restoring norder", post);
 		var prev = post.norder.prev;
 		var next = post.norder.next;
 		while(true) {
 			if (prev) {
-				if (inNorder(prev)) {
-					insertAfter(post.node, prev.node);
+				if (this.inNorder(prev)) {
+					this.insertAfter(post.node, prev.node);
 					break;
 				}
 				prev = prev.norder.prev;
 			} else if (next) {
 				next = next.norder.next;
-				if (inNorder(next)) {
-					insertBefore(post.node, next.node);
+				if (this.inNorder(next)) {
+					this.insertBefore(post.node, next.node);
 					break;
 				}
 			} else {
 				var node = post.node;
-				appendChild(node.parentNode, node);
+				this.appendChild(node.parentNode, node);
 				break;
 			}
 		}
-		setNextPost(post, null);
+		this.setNextPost(post, null);
 		post.prevpost = null;
 	}
 
-	function clearSubthread(post) {
+	M.clearSubthread = function(post) {
 		post = post.nextpost;
 		// restore subthreads to normal order
 		while(post) {
 			var next = post.nextpost;
-			undent(post);
-			restoreNorder(post);
+			this.undent(post);
+			this.restoreNorder(post);
 			post = next;
 		}
 	}
 
-	function restoreSupthread(post) {
+	M.restoreSupthread = function(post) {
 		var nextpost = post;
 		var prev;
 		post = post.prevpost;
-		while(post && isIndented(post)) {
+		while(post && this.isIndented(post)) {
 			prev = post.prevpost;
-			undent(post);
-			restoreNorder(post);
+			this.undent(post);
+			this.restoreNorder(post);
 			post = prev;
 		}
 		if (prev) {
@@ -288,7 +274,7 @@
 		}
 	}
 
-	function setNextPost(post, next) {
+	M.setNextPost = function(post, next) {
 		post.nextpost = next;
 		if (next != null) {
 			post.node.classList.add("subthread");
@@ -297,20 +283,20 @@
 		}
 	}
 
-	function inNorder(post) /*bool*/ {
+	M.inNorder = function(post) /*bool*/ {
 		return post.nextpost == null;
 	}
 
-	function isIndented(post) /*bool*/ {
+	M.isIndented = function(post) /*bool*/ {
 		return post.indented;
 	}
 
-	function undent(post) {
+	M.undent = function(post) {
 		post.node.classList.remove("indented");
 		post.indented = false;
 	}
 
-	function indent(post) {
+	M.indent = function(post) {
 		post.node.classList.add("indented");
 		post.indented = true;
 	}
@@ -328,7 +314,6 @@
 	// - highlight target of childlink
 
 })(this)
-
 
 
 
