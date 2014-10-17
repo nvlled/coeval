806983155e1d51102606d300b463041ee30329b8
ron <nvlled@gmail.com>
2014-10-17 11:48:08 +0800

    Fix possible concurrency bug

--- common/common.go
+++ common/common.go
@@ -9,17 +9,12 @@ import (
 /* Common code that can be shared
    between client and server */
 
-var rePid = regexp.MustCompile(`>>\d+`)
-
 func ParseIds(postBody string) []fora.Pid {
+    var rePid = regexp.MustCompile(`>>\d+`)
     var pids []fora.Pid
     for id := range rePid.FindAllString(postBody, 0) {
         pids = append(pids, id)
     }
     return pids
 }
-
-
-
-
 
