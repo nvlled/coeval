7d43dceb2af28965d8d663f7802dfd63ff01b267
ron <nvlled@gmail.com>
2014-10-31 22:13:35 +0800

    Add escape on html string

--- rend/common.go
+++ rend/common.go
@@ -9,26 +9,35 @@ import (
     "nvlled/coeval/urlfor"
 )
 
+const (
+    ID_PREFIX = ">>"
+)
+
 func RenderPostlinks(post fora.Post) template.HTML {
-    idRe := regexp.MustCompile(`>>\d+`)
-    brRe:= regexp.MustCompile(`\n`)
+    idRe := regexp.MustCompile(ID_PREFIX+`\d+`)
+    re := regexp.MustCompile(`\x00\x00\d+|\n`)
 
-    bs := idRe.ReplaceAllFunc([]byte(post.Body()), func(idBytes []byte) []byte {
-        id := string(idBytes[2:])
-        text := string(idBytes)
+    bs := idRe.ReplaceAllFunc([]byte(post.Body()), func(bytes []byte) []byte {
+        // replace ID_PREFIX with two zero bytes
+        // to avoid being escaped
+        return append([]byte{0, 0}, bytes[2:]...)
+    })
+    bs = []byte(template.HTMLEscapeString(string(bs)))
+    bs = re.ReplaceAllFunc(bs, func(match []byte) []byte {
+        if string(match) == "\n" {
+            return []byte("<br>")
+        }
+
+        id := string(match[2:])
         var buf bytes.Buffer
-
         link := urlfor.RouteWithPost("post-view", post, "pid", id)
         err := htmlTempl.ExecuteTemplate(&buf, "sub/postlink", Data{
             "Link" : link,
-            "Text" : text,
+            "Text" : ID_PREFIX+id,
             "Id" : id,
         })
         if err != nil { panic(err) }
         return buf.Bytes()
-    })
-    bs = brRe.ReplaceAllFunc(bs, func(_ []byte) []byte {
-        return []byte("<br>")
     })
     return template.HTML(bs)
 }
