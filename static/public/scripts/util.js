a16bec2c3d14a252b35ec017de2626d26b7177e4
ron <nvlled@gmail.com>
2014-11-10 16:06:38 +0800

    Add util functions

--- static/public/scripts/util.js
+++ static/public/scripts/util.js
@@ -4,6 +4,8 @@
 
     var M = {};
 
+    var reduce = Array.prototype.reduceRight;
+
     M.fetch = function(url, fn) {
         var req = new XMLHttpRequest();
         req.open("GET", url);
@@ -11,6 +13,89 @@
             fn(data);
         }
         req.send();
+    }
+
+    M.formValues = function(form) {
+        var fields = form.querySelectorAll("[name]");
+        return reduce.call(fields, function(result, field) {
+            if (field.type != "hidden") {
+                result[field.name] = field.value;
+            }
+            return result;
+        }, {});
+    }
+
+    M.encodeFormData = function(data) {
+        var result = "";
+        for (var k in data) {
+            var key = encodeURIComponent(k);
+            var value = encodeURIComponent(data[k]);
+            result += key+"="+value+"&";
+        }
+        return result;
+    }
+
+    M.asynchronizeForm = function(form, opts) {
+        opts = opts || {};
+
+        var url = new URL(form.action);
+        var method = form.method;
+        var info = form.querySelector(".info");
+
+        url.pathname = "/api"+url.pathname;
+
+        form.onsubmit = function(e) {
+            e.preventDefault();
+            clearErrors(form);
+
+            var data = M.formValues(form);
+            console.log("submitting form", M.encodeFormData(data));
+
+            if (info) {
+                info.textContent = "submitting..."
+            }
+            var req = new XMLHttpRequest();
+            req.open(method, url.toString());
+            req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            req.onload = function() {
+                if (req.status / 100 != 2)
+                    showError(req);
+                else {
+                    info.textContent = "Done";
+                    form.reset();
+                    if (typeof opts.handler === "function")
+                        opts.handler(req.responseText);
+                }
+            }
+            req.onerror = function() {
+                info.textContent = "submission failed";
+            }
+            req.send(M.encodeFormData(data));
+            return false;
+        }
+
+        function clearErrors(form) {
+            var errors = form.querySelectorAll(".error");
+            for (var i = 0; i < errors.length; i++) {
+                errors[i].textContent = "";
+            }
+        }
+
+        function showError(req) {
+            try {
+                var resp = JSON.parse(req.responseText);
+                var error = resp.error;
+                if (error) {
+                    console.log("error", error);
+                    for (var k in error) {
+                        var out = form.querySelector(".error."+k);
+                        if (out)
+                            out.textContent = ">"+error[k];
+                    }
+                }
+            } catch (e) { }
+            info.textContent = "submission failed";
+        }
     }
 
     root.util = M;
